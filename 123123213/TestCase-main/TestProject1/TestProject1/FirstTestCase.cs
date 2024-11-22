2090b8b4134cc8586249373e2c94d2aa1d297e0f
Opilane TTHK <opilane@tthk.ee>
2024-11-22 12:03:10 +0200

    a

--- 123123213/TestCase-main/TestProject1/TestProject1/FirstTestCase.cs
+++ 123123213/TestCase-main/TestProject1/TestProject1/FirstTestCase.cs
@@ -1,54 +1,40 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
+using System.Xml.Linq;
 
-namespace TestProject1
+namespace SeleniumMarken
 {
-    public  class FirstTestCase
+    public class FirstTestCase
     {
+        public event EventHandler? Clicked;
+
         static void Main(string[] args)
         {
+            //TestHidingButton();
+            //TestClickTwice();
+            //TestHiddenButtons();
+            //TestClick();
+            //TestWaitForButton();
+            //TestTextInput();
+        }
+        [Test]
+        public static void TestHidingButton()
+        {
+
             string binaryLocation = "C:\\Users\\opilane\\source\\repos\\FFP\\FirefoxPortable\\App\\Firefox64\\Firefox.exe";
+
 
             FirefoxOptions options = new FirefoxOptions();
             options.BrowserExecutableLocation = binaryLocation;
-            
-            
-            IWebDriver driver = new FirefoxDriver("C:\\Users\\opilane\\source\\repos\\123123213\\TestCase-main\\TestProject1\\TestProject1\\drivers", options);
-            driver.Url = "file:///C:/Users/opilane/source/repos/PEEEEETR/ARLOOO-main/Shoppp-main/M-ng-main/index.html";
-        }
-        [Test]
-        public void TestClick()
-        {
-            driver.Navigate().GoToUrl("http://www.uitestingplayground.com/click");
 
-            var Button = driver.FindElement(By.Id("badButton"));
 
-            Actions actions = new Actions(driver);
-            actions.MoveToElement(Button).Click().Perform();
+            IWebDriver driver = new FirefoxDriver("C:\\Users\\opilane\\source\\repos\\eeeeeeee-main\\123123213\\TestCase-main\\TestProject1\\TestProject1\\drivers", options);
 
-            string buttonClass = Button.GetAttribute("class");
-            Assert.That(buttonClass.Contains("btn-success"), Is.True, "The button did not turn green after clicking.");
-        }
-        [Test]
-        public void TestAlerts()
-        {
-            driver.Navigate().GoToUrl("http://www.uitestingplayground.com/alerts");
+            driver.Url = "http://www.uitestingplayground.com/scrollbars";
 
-            var alertButton = driver.FindElement(By.Id("alertButton"));
-            alertButton.Click();
-            driver.SwitchTo().Alert().Accept();
-
-            var confirmButton = driver.FindElement(By.Id("confirmButton"));
-            confirmButton.Click();
-            Thread.Sleep(1000);
-            driver.SwitchTo().Alert().Accept();
-
-            var promptButton = driver.FindElement(By.Id("promptButton"));
-            promptButton.Click();
-            Thread.Sleep(1000);
-            var alert = driver.SwitchTo().Alert();
-            alert.SendKeys("hello");
-            alert.Accept();
+            IWebElement element = driver.FindElement(By.Id("hidingButton"));
+            element.Click();
         }
         [Test]
         public static void TestClickTwice()
@@ -58,12 +44,53 @@ namespace TestProject1
             FirefoxOptions options = new FirefoxOptions();
             options.BrowserExecutableLocation = binaryLocation;
 
-            IWebDriver driver = new FirefoxDriver("C:\\Users\\opilane\\source\\repos\\123123213\\TestCase-main\\TestProject1\\TestProject1\\drivers", options);
+            IWebDriver driver = new FirefoxDriver("C:\\Users\\opilane\\source\\repos\\eeeeeeee-main\\123123213\\TestCase-main\\TestProject1\\TestProject1\\drivers", options);
             driver.Url = "http://www.uitestingplayground.com/mouseover";
 
             Actions actions = new Actions(driver);
             IWebElement element = driver.FindElement(By.ClassName("text-primary"));
             actions.DoubleClick(element).Perform();
+
+        }
+        [Test]
+
+        public static void TestClientDelay()
+        {
+            string binaryLocation = "C:\\Users\\opilane\\source\\repos\\FFP\\FirefoxPortable\\App\\Firefox64\\Firefox.exe";
+
+            FirefoxOptions options = new FirefoxOptions();
+            options.BrowserExecutableLocation = binaryLocation;
+
+            IWebDriver driver = new FirefoxDriver("C:\\Users\\opilane\\source\\repos\\eeeeeeee-main\\123123213\\TestCase-main\\TestProject1\\TestProject1\\drivers", options);
+            driver.Url = "http://www.uitestingplayground.com/clientdelay";
+
+            IWebElement element = driver.FindElement(By.Id("ajaxButton"));
+            element.Click();
+            Thread.Sleep(15000);
+            IWebElement p = driver.FindElement(By.ClassName("bg-success"));
+            p.Click();
+        }
+        [Test]
+        public static void TestClick()
+        {
+            string binaryLocation = "C:\\Users\\opilane\\source\\repos\\FFP\\FirefoxPortable\\App\\Firefox64\\Firefox.exe";
+
+            FirefoxOptions options = new FirefoxOptions();
+            options.BrowserExecutableLocation = binaryLocation;
+
+            IWebDriver driver = new FirefoxDriver("C:\\Users\\opilane\\source\\repos\\eeeeeeee-main\\123123213\\TestCase-main\\TestProject1\\TestProject1\\drivers", options);
+            driver.Url = "http://www.uitestingplayground.com/click";
+
+            IWebElement element = driver.FindElement(By.Id("badButton"));
+            element.Click();
+            try
+            {
+                element.Click();
+            }
+            catch
+            {
+                Console.WriteLine("Cannot click button");
+            }
 
         }
         [Test]
@@ -74,7 +101,7 @@ namespace TestProject1
             FirefoxOptions options = new FirefoxOptions();
             options.BrowserExecutableLocation = binaryLocation;
 
-            IWebDriver driver = new FirefoxDriver("C:\\Users\\opilane\\source\\repos\\123123213\\TestCase-main\\TestProject1\\TestProject1\\drivers", options);
+            IWebDriver driver = new FirefoxDriver("C:\\Users\\opilane\\source\\repos\\eeeeeeee-main\\123123213\\TestCase-main\\TestProject1\\TestProject1\\drivers", options);
             driver.Url = "http://www.uitestingplayground.com/autowait";
 
             IWebElement element = driver.FindElement(By.Id("applyButton3"));
@@ -84,42 +111,21 @@ namespace TestProject1
             element2.Click();
 
         }
-        public void TextInput()
-        {
-            driver.Navigate().GoToUrl("http://www.uitestingplayground.com/textinput");
-
-            var inputField = driver.FindElement(By.Id("newButtonName"));
-            var button = driver.FindElement(By.Id("updatingButton"));
-
-            Actions actions = new Actions(driver);
-            actions.Click(inputField)
-                .SendKeys("Hello World")
-                .Perform();
-
-            button.Click();
-
-            string buttonText = button.Text;
-            Assert.That(buttonText, Is.EqualTo("Hello World"), "The button name did not change as expected.");
-        }
         [Test]
-        public void TestDisabledInput()
+        public static void TestUpload()
         {
-            driver.Navigate().GoToUrl("http://www.uitestingplayground.com/disabledinput");
+            string binaryLocation = "C:\\Users\\opilane\\source\\repos\\FFP\\FirefoxPortable\\App\\Firefox64\\Firefox.exe";
 
-            var button = driver.FindElement(By.Id("enableButton"));
-            button.Click();
+            FirefoxOptions options = new FirefoxOptions();
+            options.BrowserExecutableLocation = binaryLocation;
 
-            var inputField = driver.FindElement(By.Id("inputField"));
-            var waitTime = TimeSpan.FromSeconds(0);
-            while (inputField.GetAttribute("disabled") == "true" && waitTime.TotalSeconds < 10)
-            {
-                Thread.Sleep(500);
-                waitTime += TimeSpan.FromMilliseconds(500);
-            }
+            IWebDriver driver = new FirefoxDriver("C:\\Users\\opilane\\source\\repos\\eeeeeeee-main\\123123213\\TestCase-main\\TestProject1\\TestProject1\\drivers", options);
+            driver.Url = "http://www.uitestingplayground.com/upload";
 
-            inputField.SendKeys("Hello World!");
+            IWebElement fileInput = driver.FindElement(By.Id("file-upload"));
+            string filePath = @"C:\path\to\your\file.txt";
+            fileInput.SendKeys(filePath);
 
-            Assert.That(inputField.GetAttribute("value"), Is.EqualTo("Hello World!"), "The input field value was not set correctly.");
         }
     }
 }
